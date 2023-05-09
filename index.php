7fbf37ac683659c3289f54c97a330af1d406f4f0
thankuxari <122522860+thankuxari@users.noreply.github.com>
2023-05-10 00:44:17 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,70 @@
-<?php
-    
-    $cmd = "java -classpath weka.jar weka.classifiers.bayes.NaiveBayes -T new-immunotherapy.arff -l test4.model -p 0 2>&1";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="avatar.png">
+    <title>Ανοσοθεραπεία | Συστήματα Υποστήριξης Αποφάσεων </title>
+</head>
+<body>
+    <main>
+        <form method="post">
+            sex: <input type="text" name="sex">
+            age: <input type="number" name="age">
+            time: <input type="number" name="time">
+            Number of Warts: <input type="number" name="warts">
+            Type: <input type="number" name="type">
+            Area: <input type="number" name="area">
+            induration diameter: <input type="number" name="induration">
+            <input type="submit" name="submit">
+        </form>
+        <?php
+        
+            if(isset($_POST['submit']))
+            {
+                $value1 = $_POST['sex'];
+                $value2 = $_POST['age'];
+                $value3 = $_POST['time'];
+                $value4 = $_POST['warts'];
+                $value5 = $_POST['type'];
+                $value6 = $_POST['area'];
+                $value7 = $_POST['induration'];
 
-    exec($cmd,$output);
+                $fp = fopen('new-immunotherapy.arff','w');
 
-    echo "<h3>Results...</h3>";
-    for($i=0;$i<sizeof($output);$i++)
-    {
-        trim($output[$i]);
-        echo($output[$i])."<br>";
-    }
-?>
+                if($fp)
+                {
+                    $str = 
+                    "@relation Immunotherapy
+@attribute sex numeric
+@attribute age numeric
+@attribute Time numeric
+@attribute Number_of_Warts numeric
+@attribute Type numeric
+@attribute Area numeric
+@attribute induration_diameter numeric
+@attribute Result_of_Treatment {0,1}
+                    
+                    @data \n".$value1.",".$value2.",".$value3.",".$value4.",".$value5.",".$value6.",".$value7.",?\n";
+                    if(!fwrite($fp,$str)) die ("couldnt write to file.");
+                }
+                fclose($fp);
+            }        
+            
+            $cmd = "java -classpath weka.jar weka.classifiers.bayes.NaiveBayes -T new-immunotherapy.arff -l test5.model -p 0 2>&1";
+
+            exec($cmd,$output);
+
+            echo "<h3>Results...</h3>";
+            for($i=0;$i<sizeof($output);$i++)
+            {
+                trim($output[$i]);
+                echo($output[$i])."<br>";
+            }
+        ?>
+    </main>
+</body>
+</html>
+
